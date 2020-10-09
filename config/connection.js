f450a92500d08d534ef59d05266871fa21e7ceee
dfabert <glen.fabert@gmail.com>
2020-10-08 20:58:24 -0400

    fixed error in connection.js and added deployed picture

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,13 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection([
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "dfwebdev",
     password: "go8of4y",
     database: "burgers_db"
-  ]);
+  });
 };
 
 connection.connect(function(err) {
